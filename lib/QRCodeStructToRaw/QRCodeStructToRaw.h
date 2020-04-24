afd21366d3a6a841bd05efe524d05ddf7ebe0d5b
gabriel <gabrielgoller123@gmail.com>
2020-04-24 18:49:41 +0200

    updated makefile with -Wall and made function not visible to global changed function to static because it is only needed in that function defined mapAlphanumericalCharacters in c file

--- lib/QRCodeStructToRaw/QRCodeStructToRaw.h
+++ lib/QRCodeStructToRaw/QRCodeStructToRaw.h
@@ -8,11 +8,9 @@ int QRCodeRawData[];
 int lengthRawData;
 
 // Map characters to special numbers(not ascii! alphanumerical code)
-static const char mapAlphanumericalCharacters[45] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', \
-'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '$', '%', '*', '+', '-', '.', '/', ':'};
+const char mapAlphanumericalCharacters[45];
 
-// Returns index of searched char in mapCharacters
-int getIndexInMapCharacters(char query);
+//int getIndexInMapCharacters(char query);
 
 // Converts struct with data to array with raw data
 // Adds hamming codes to data
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 compile: src/main.c lib/QRCodeStructToRaw/QRCodeStructToRaw.c lib/binaryConverter/binaryConverter.c
-	gcc src/main.c lib/QRCodeStructToRaw/QRCodeStructToRaw.c lib/binaryConverter/binaryConverter.c
+	gcc -Wall src/main.c lib/QRCodeStructToRaw/QRCodeStructToRaw.c lib/binaryConverter/binaryConverter.c
 
 test: test/test.c lib/QRCodeStructToRaw/QRCodeStructToRaw.c lib/binaryConverter/binaryConverter.c
-	gcc test/test.c lib/QRCodeStructToRaw/QRCodeStructToRaw.c lib/binaryConverter/binaryConverter.c
+	gcc -Wall test/test.c lib/QRCodeStructToRaw/QRCodeStructToRaw.c lib/binaryConverter/binaryConverter.c
 
 clean: 
 	rm -f *.out
