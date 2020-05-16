09e2d1f7f61577125f38954951a92d9fc4b45811
Gavaii <61867970+Gavaii@users.noreply.github.com>
2020-05-16 11:42:47 +0200

    Written h-file

---
+++ lib/drawQr/drawQr.h
@@ -0,0 +1,27 @@
+#ifndef _DRAWQR_H_
+#define _DRAWQR_H_
+
+/// Struct to store Binary Number
+struct BinaryNumber {
+    /// Stores binary array
+    int array[11];
+    /// Stores length of array
+    int length;
+};
+
+/** Converts a decimal Number into a BinaryNumber struct.
+ *  @param[in] number decimal number to convert
+ *  @param[in] length length of desired output array (rest is filled with bits)
+ *  @return BinaryNumber struct with binary number
+ */ 
+struct BinaryNumber decToBin(int number, int length);
+
+
+/** Converts binary number in decimal number
+ *  @param[in] binaryNumber array with binary number
+ *  @param[in] length length of binary number
+ *  @return number converted decimal Number
+ */ 
+int binToDec(int binaryNumber[], int lenght);
+
+#endif
