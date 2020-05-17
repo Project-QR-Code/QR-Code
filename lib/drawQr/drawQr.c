fe111e14bb94128830d7f31bce2e969e408729bf
Gavaii <61867970+Gavaii@users.noreply.github.com>
2020-05-17 15:35:53 +0200

    Fixed some bugs. Better Data-handling

--- lib/drawQr/drawQr.c
+++ lib/drawQr/drawQr.c
@@ -14,8 +14,9 @@ int insertdata(int array[]){
 void prepareRectangle(){
     for(int j=0;j<3;j++){
 		int sizey,sizex;
-		int alternate=1;
+		int alternate=5;
 		int startx,starty;
+		int county,countx;
 		if(j==0){
 			starty=0;
 			startx=0;
@@ -36,23 +37,67 @@ void prepareRectangle(){
 		}
 		
 		
-		for(int counter=1;counter<4;counter++){  			//3x positionserkennung
+		for(int counter=1;counter<4;counter++){  			//3x positionserkennungsfeld
 			for(int y=starty;y<(sizey);y++){
 				for(int x=startx;x<(sizex);x++){
 					rectangle[y][x]=alternate;
 				}
 			}
-			if(alternate==1)
+			if(alternate==5)
 				alternate--;
-			else
+			else if(alternate==4)
 				alternate++;
-			
+			else
+				printf("Error");
+
 			sizex--;
 			sizey--;
 			starty++;
 			startx++;
 		}
 	}
+	for(int times=0;times<3;times++){
+		int sizey,sizex;
+		int county,countx;
+		if(times==0){
+			county=0;
+			countx=0;
+			sizey=8;
+			sizex=8;
+		}else if(times==1){
+			county=0;
+			countx=13;
+			sizey=8;
+			sizex=21;
+		}else if(times==2){
+			county=13;
+			countx=0;
+			sizey=21;
+			sizex=8;
+		}
+		
+		for(int y=county;y<sizey;y++){			//weiße "rahmen" der felder
+			for(int x=countx;x<sizex;x++){
+				if(rectangle[y][x]==0)
+					rectangle[y][x]=4;
+			}
+		}
+		
+		
+	}
+	rectangle[6][8]=5;			//linien, die die felder(oben) verbinden
+	rectangle[6][10]=5;
+	rectangle[6][12]=5;
+	rectangle[6][9]=4;
+	rectangle[6][11]=4;
+	
+	rectangle[8][6]=5;
+	rectangle[10][6]=5;
+	rectangle[12][6]=5;
+	rectangle[9][6]=4;
+	rectangle[11][6]=4;
+	
+	rectangle[13][8]=5;			//punkt links unten
 }
 void drawRectangle(int data){
     
