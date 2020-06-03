012c0a24092890ba27f79fa816fcd8df06234e4a
Gavaii <61867970+Gavaii@users.noreply.github.com>
2020-06-03 16:48:41 +0200

    Cleanup

--- lib/drawQr/drawQr.h
+++ lib/drawQr/drawQr.h
@@ -1,7 +1,9 @@
 #ifndef _DRAWQR_H_
 #define _DRAWQR_H_
-#define LENGHT 21   //our version has 21x21 size
+#define LENGHT 21 //our version has 21x21 size
 
-
+int insertData();
+void prepareRectangle();
+int drawRectangle();
 
 #endif
--- tests/test.c
+++ tests/test.c
@@ -1,24 +1,27 @@
-#include "../lib/QRCode/model.h"
-#include "../lib/QRCodeStructToRaw/QRCodeStructToRaw.h"
-#include "../lib/reedSolomon/reedSolomon.h"
+//#include "../lib/QRCode/model.h"
+//#include "../lib/QRCodeStructToRaw/QRCodeStructToRaw.h"
+//#include "../lib/reedSolomon/reedSolomon.h"
 #include "../lib/drawQr/drawQr.h"
-#include "../lib/drawQr/createPBMImage.h"
+//#include "../lib/drawQr/createPBMImage.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+int data[] = {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0};
 
+//int rectangle[21][21]={};
 
 int main(){
     // TESTING!
-    struct QRCode test;
-    test.codificationId[0] = 0;
+    //struct QRCode test;
+    /*test.codificationId[0] = 0;
     test.codificationId[1] = 0;
     test.codificationId[2] = 1;
     test.codificationId[3] = 0;
     test.lengthOfData = 4;
     char *test_string = "TEST";
-    strcpy(test.data, test_string);
-    convertQrStructToRaw(test);
+    strcpy(test.arrdata, test_string);
+    convertQrStructToRaw(test);*/
     printf("\n");
-    //insertData(QRCodeRawData);
+    insertData();
+    
 }
