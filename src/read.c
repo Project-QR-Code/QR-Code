2ea3ef9f712e055f8b15f6411bf28899683cc190
gabriel <gabrielgoller123@gmail.com>
2020-06-06 14:26:34 +0200

    🖉Started writing read.c, reading from bitmap image is missing

--- src/read.c
+++ src/read.c
@@ -1,9 +1,17 @@
 #include "../lib/QRCode/model.h"
-#include "../lib/QRCodeStructToRaw/QRCodeStructToRaw.h"
+#include "../lib/rawToQrCodeStruct/rawToQrCodeStruct.h"
 #include <stdio.h>
 #include <string.h>
 
 int main(){
-    
+    // Read from bitmap image
+    // Currently developed in branch readQr
+    //TODO
+
+    int bitstream[208];
+    int bitstream_length = 208;
+    struct QRCode output;
+    output = decodeRawData(bitstream, bitstream_length);
+    printf("String: %s\n", output.data);
     return 0;
 }
